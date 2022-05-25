14192fa5c5e9138f073552df5f85f050ef458091
Levkovich_Ekaterina <katya7476803@gmail.com>
2022-05-25 19:52:28 +0300

    editing style

--- src/pages/no-match.jsx
+++ src/pages/no-match.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable arrow-body-style */
 export const NoMatch = () => {
   return (
-    <>
+    <div className="noMatch">
       <div>UPS</div>
       <div>Page not found</div>
-    </>
+    </div>
   );
 };
